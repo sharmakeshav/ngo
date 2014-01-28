6fc92f364405eadebac3e214438a6263063d7903
sharmakeshav <keshav9208@gmail.com>
2014-01-28 22:02:55 +0530

    Uploaded files.

---
+++ contact.php
@@ -0,0 +1,95 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Nirbhaya | Contact Us</title>
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+        <link rel="shortcut icon" type="image/png" href="favicon.png">
+        <link rel="stylesheet" href="css/index.css"/>
+        <link rel="stylesheet" href="css/contact.css"/>
+    </head>
+    <body>
+
+        <a href="index.html"><img id="logo1" src="img/logo.png" alt="" width="210" height="55"/></a>
+        <div id="contain">
+
+            <div class="nav">
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                </ul>
+            </div>
+            <div style="width: 140px;" class="nav">
+                <ul>
+                    <li style="width: 120px;"><a href="news.html">Photo Gallery</a></li>
+                    <li style="width: 120px;"><a href="program.html">Programs</a></li>
+                </ul>
+            </div>
+            <div class="nav">
+                <ul>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+        <div id="body"></div>
+        <div id="details">
+            <label>Nirbhaya Organisation</label><br/><br/>
+            <label>N-20 Kasturba Gandhi Marg<br/>New Delhi-110001</label><br/>
+            <br/>
+            <label>8870509930</label><br/>
+            <label>9042303036</label><br/>
+            <br/>
+            <label>mailus@nirbhaya.com</label><br/>
+            <br/>
+            <label><strong>Opening Hours</strong></label><br/>
+            <label>Monday to Friday</label><br/>
+            <label>09:00 hours to 17:00 hours</label>
+        </div>
+        <div id="main">
+        <div id="con">
+            <label>
+                Drop us a Message
+            </label>
+        </div>
+        <div id="back-con">
+            We would love to talk to you and answer all your questions
+        </div>
+        <form>
+            <div id="contact">
+                <table>
+                    <tr><td><label>Name</label></td></tr>
+                    <tr><td><input type="text" placeholder="Your Name" style="height:46px;width:480px;" required  pattern="^[a-zA-Z0-9]{4,20}$"/></td></tr>
+                    <tr><td><label>Email</label></td></tr>
+                    <tr><td><input type="text" placeholder="xyz@gmail.com" style="height:46px;width:480px;" required/></td></tr>
+                    <tr><td><label>Message</label></td></tr>
+                    <tr><td><textarea cols="100" rows="8" style="height:195px;width:750px;" placeholder="Type your message here" required></textarea></td></tr>
+                    <tr><td><button id="button">Send it</button></td></tr>
+                </table>
+            </div>
+        </form>
+        </div>
+        <div id="footer">
+           <a href="http://facebook.com" class="links">
+            <span class="icon-facebook" data-icon="&#xe005;"> </span>
+           </a>
+           <a href="http://twitter.com" class="links">
+            <span class="icon-twitter" data-icon="&#xe003;"> </span>
+            </a>
+            <a href="http://plus.google.com" class="links">
+             <span class="icon-google-plus" data-icon="&#xe000;"> </span>
+            </a>
+            <a href="#" class="links">
+             <span class="icon-feed" data-icon="&#xe006;"> </span>
+            </a>
+            <a href="contact.html" class="navbar">Contact Us</a>
+            <a href="blog.html" class="navbar">Blog</a>
+            <a href="program.html" class="navbar">Programs</a>
+            <a href="news.html" class="navbar">Photo Gallery</a>           
+            <a href="about.html" class="navbar">About</a>
+            <a href="index.html" class="navbar">Home</a>
+        </div>
+        <!--<label id="rit">&copy; Nirbhaya 2013</label>-->
+    </body>
+</html>
+
